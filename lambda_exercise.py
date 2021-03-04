51698ad872b08a825fcf208ae289bef74715c2ee
huoyan298 <YTestEamil@163.com>
2021-03-04 15:51:44 +0800

    lambda

--- lambda_exercise.py
+++ lambda_exercise.py
@@ -10,11 +10,19 @@ print(g(1))
 print(g(2))
 #2.
 foo=[2,18,9,22,16,17,24,8,12,27]
-# print((filter(lambda x:x %3==0,foo)))
-# print[x for x in foo if x % 3 ==0]
+# filter输出后是一数组而非列表
+print("lambda:"+str(list(filter(lambda x:x %3==0,foo))))
+print([x for x in foo if x % 3 ==0])
 #3
 f = lambda x,y,z:x+y+z
 print(f(1,2,3))
 
 L=[lambda x:x+2,lambda x:x*2,lambda x:x**2]
 print("L=",L[0](1),L[1](2),L[2](3))
+
+#4
+def inc(x):
+    return x+10
+
+I=[1,2,3]
+print(map(inc,I))
