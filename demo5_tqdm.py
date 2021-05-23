50c6be30cb2c837e273ffb998241f4c342ddc324
huoyan298 <YTestEamil@163.com>
2021-05-23 21:10:42 +0800

    demo tqdm

---
+++ demo5_tqdm.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 
+# @Author  : huoyanyang
+# @Site    : 
+# @File    : 
+# @Software: PyCharm
+import time
+from tqdm import tqdm
+total =100
+for _ in tqdm(range(total)):
+    time.sleep(0.05)
